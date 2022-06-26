b26a37459cc99463a8267dc7ef6e3b3f97e40efb
Vineet Parvathala <vparv@umich.edu>
2022-06-25 20:57:23 -0400

    Finished basic Solidity functionality - stake, upload, vote, withdraw

--- contracts/Bounty.sol
+++ contracts/Bounty.sol
@@ -4,18 +4,31 @@ pragma solidity ^0.8.7;
 //import "./PriceConverter.sol";
 
 contract Bounty {
+    //using PriceConverter for uint256;
+    // stake, vote, upload
+    // Create an array
     videoInfo[] public videos;
     address[] stakers;
     uint256 minStakeAmount = 0;
-
     string public request;
     uint public deadline;
+
     // Video Address/String -> Struct of Video Information - for Vote Addresses
     // Staked Address -> Amount Staked
-    mapping(address => uint256) public amountStaked;
+    mapping(address => uint256) public addressToAmount;
+    mapping(address => bool) public voterAddresses;
     // money sent to creator address
     address winnerAddr = 0x09FD51F989179a4B04d318F8ae76CFD22c447515;
 
+    uint256 winningIdx;
+    bool winnerAssigned = false;
+
+    // Highest vote count - this is a running stat
+    uint256 highestCount = 0;
+
+    uint256[] public votes;
+
+    // ID for each video is index in videos array
     struct videoInfo {
         address[] voterAddresses;
         address submissionAddr;
@@ -24,6 +37,7 @@ contract Bounty {
 
     constructor(string memory _request, uint _deadlineTime) {
         request = _request;
+        // Used to calculate when payment withdraw for winning
         deadline = block.timestamp + _deadlineTime;
     }
 
@@ -34,24 +48,44 @@ contract Bounty {
             "You must stake atleast the original stake in order to join this bounty!"
         );
         stakers.push(msg.sender);
-        amountStaked[msg.sender] = msg.value;
+        addressToAmount[msg.sender] = msg.value;
+        voterAddresses[msg.sender] = true;
+        minStakeAmount = msg.value;
     }
 
     function withdraw() public payable {
+        require(
+            block.timestamp >= deadline,
+            "This bounty is still ongoing. Please try after the deadline has passed."
+        );
+
+        winnerAddr = videos[winningIdx].submissionAddr;
         payable(winnerAddr).transfer(address(this).balance);
     }
 
+    function getWinner() public view returns (uint256) {
+        return winningIdx;
+    }
+
+    function getMinStake() public view returns (uint256) {
+        return minStakeAmount;
+    }
+
+    // Used to find total amount staked
     function getBalance() public view returns (uint) {
         return address(this).balance;
     }
 
-    function upload(address _submissionAddr, string memory _assetLink)
-        public
-        pure
-    {
+    function upload(string memory _assetLink) public {
+        require(
+            !voterAddresses[msg.sender],
+            "You do not have the ability to upload videos on this!"
+        );
         videoInfo memory newSubmission;
-        newSubmission.submissionAddr = _submissionAddr;
+        newSubmission.submissionAddr = msg.sender;
         newSubmission.assetLink = _assetLink;
+        videos.push(newSubmission);
+        votes.push(0);
     }
 
     function setStakeAmt(uint _minStakeAmount) public returns (uint) {
@@ -88,5 +122,33 @@ contract Bounty {
 
     receive() external payable {
         stake();
+    }
+
+    function vote(uint idx) public {
+        require(
+            voterAddresses[msg.sender],
+            "You do not have the ability to vote on this!"
+        );
+        voterAddresses[msg.sender] = false;
+        votes[idx] = votes[idx] + 1;
+
+        if (votes[idx] > highestCount) {
+            winningIdx = idx;
+            highestCount = votes[idx];
+            winnerAssigned = true;
+        }
+    }
+
+    // Only run after deadline has closed, and submissions should be stopped. Awards money after.
+    function tallyVotes() public {
+        require(
+            winnerAssigned,
+            "There were no votes cast. The stake will be returned to the stakers."
+        );
+        require(
+            highestCount > 0,
+            "There were no votes cast. The stake will be returned to the stakers."
+        );
+        withdraw();
     }
 }
